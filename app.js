343b1dd92573c21ad18cb80ac1694a87baa387e4
Ruth Zabaleta <ruth.zabaleta98@gmail.com>
2023-10-07 02:56:36 -0300

    proyeto final

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var shopRouter = require('./routes/shop');
+var sobremiRouter = require('./routes/sobremi');
+var contactoRouter = require('./routes/contacto');
 
 var app = express();
 
@@ -21,6 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/shop', shopRouter);
+app.use('/sobremi', sobremiRouter);
+app.use('/contacto', contactoRouter); 
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
